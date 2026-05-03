ad97fefac9b827c2c18d8910a8020d7fba3ff9eb
raodevlopers <raoscomedy@gmail.com>
2026-05-03 09:43:19 +0530

    chore: remove temp debug scripts

--- fix_loading_2.py
+++
@@ -1,9 +0,0 @@
-
-with open('team-member-pwa/src/pages/DashboardPage.jsx', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace('{loading ? Array(2).fill(0).map((_, i) => (\n  <Card key={i} className=\"min-w-[240px]\"', '{projLoading ? Array(2).fill(0).map((_, i) => (\n  <Card key={i} className=\"min-w-[240px]\"')
-
-with open('team-member-pwa/src/pages/DashboardPage.jsx', 'w', encoding='utf-8') as f:
-    f.write(content)
-
